a4ee5a4da8da7964e262b40caaff1af3dc75648a
Clive Mogusu <cmogusu@gmail.com>
2025-07-12 01:03:24 +0300

    add simple dashboard

---
+++ src/app/maplibre/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Page() {
+	return (
+		<div>
+			<h2>maplibre</h2>
+			<Link href="/">maplibre</Link>
+		</div>
+	);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+"use client";
+
+import { DashboardLayout } from "@toolpad/core/DashboardLayout";
+import { PageContainer } from "@toolpad/core/PageContainer";
+import Paper from "@mui/material/Paper";
 import { App } from "../pages/Hello";
 
 export default function Home() {
 	return (
-		<main>
-			<header>Hellow</header>
-			<App />
-		</main>
+		<DashboardLayout>
+			<Paper sx={{ width: "100%" }}>
+				<PageContainer>
+					<header>Hellow</header>
+				</PageContainer>
+				<App />
+			</Paper>
+		</DashboardLayout>
 	);
 }
