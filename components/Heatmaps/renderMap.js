95ecbf319724194b5193ac5eeed17ddfd2b53572
Clive Mogusu <cmogusu@gmail.com>
2025-07-17 15:57:00 +0300

    added cluster points

--- components/Heatmaps/renderMap.js
+++ components/Heatmaps/renderMap.js
@@ -26,116 +26,121 @@ export function renderMap(container, width, height, mapTilerApiKey) {
   );
 
   map.on("load", () => {
+    const firstSymbolId = getFirstSymbol(map);
+
     map.addSource("development-activity-source", {
       type: "geojson",
       data: "https://clived.live/development-activity-monitor.geojson",
+      cluster: true,
+      clusterMaxZoom: 14,
+      clusterRadius: 50,
     });
 
-    map.addLayer({
-      id: "development-activity-layer",
-      type: "circle",
-      source: "development-activity-source",
-      paint: {
-        "circle-radius": [
-          "interpolate",
-          ["linear"],
-          ["zoom"],
-          4,
-          1,
-          6,
-          1.5,
-          8,
-          2,
-          12,
-          2.5,
-          16,
-          5,
-        ],
-        "circle-opacity": 0.8,
-        "circle-color": [
-          "interpolate",
-          ["linear"],
-          ["zoom"],
-          1,
-          "rgba(33,102,172,0)",
-          2,
-          "rgb(103,169,207)",
-          3,
-          "rgb(209,229,240)",
-          4,
-          "rgb(253,219,199)",
-          5,
-          "rgb(239,138,98)",
-          6,
-          "rgb(178,24,43)",
-        ],
+    map.addLayer(
+      {
+        id: "development-activity-layer",
+        type: "circle",
+        source: "development-activity-source",
+        paint: {
+          "circle-radius": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            4,
+            1,
+            6,
+            1.5,
+            8,
+            2,
+            12,
+            2.5,
+            16,
+            5,
+          ],
+          "circle-opacity": 0.8,
+          "circle-color": "#ff0000",
+          // "circle-color": [
+          //   "interpolate",
+          //   ["linear"],
+          //   ["zoom"],
+          //   8,
+          //   "rgba(33,102,172,0)",
+          //   12,
+          //   "rgb(103,169,207)",
+          //   14,
+          //   "rgb(209,229,240)",
+          //   16,
+          //   "rgb(253,219,199)",
+          //   20,
+          //   "rgb(239,138,98)",
+          //   22,
+          //   "rgb(178,24,43)",
+          // ],
+        },
+        filter: ["==", "$type", "Point"],
+        minzoom: 9,
+        maxzoom: 24,
       },
-      filter: ["==", "$type", "Point"],
-    });
+      firstSymbolId
+    );
 
-    // map.addLayer({
-    //   id: 'developments-labels',
-    //   type: 'symbol',
-    //   source: 'developments',
-    //   layout: {
-    //     'text-field': ['get', 'development_key'],
-    //     'text-size': 12,
-    //     'text-offset': [0, 1.2],
-    //     'text-anchor': 'top'
-    //   },
-    //   paint: {
-    //     'text-color': '#333',
-    //     'text-halo-color': '#fff',
-    //     'text-halo-width': 1
-    //   }
-    // });
+    map.addLayer(
+      {
+        id: "clusters",
+        type: "circle",
+        source: "development-activity-source",
+        filter: ["has", "point_count"],
+        paint: {
+          "circle-color": [
+            "step",
+            ["get", "point_count"],
+            "#51bbd6",
+            100,
+            "#f1f075",
+            750,
+            "#f28cb1",
+          ],
+          "circle-radius": [
+            "step",
+            ["get", "point_count"],
+            10,
+            50,
+            20,
+            750,
+            30,
+          ],
+        },
+      },
+      firstSymbolId
+    );
 
-    // map.addLayer({
-    //   "id": "Point labels",
-    //   "type": "symbol",
-    //   "source": "development-activity-layer",
-    //   "layout": {
-    //     "symbol-placement": "line",
-    //     "symbol-spacing": [
-    //       "step",
-    //       [
-    //         "zoom"
-    //       ],
-    //       250,
-    //       21,
-    //       900
-    //     ],
-    //     "text-field": [
-    //       "coalesce",
-    //       [
-    //         "get",
-    //         "name:en"
-    //       ],
-    //       [
-    //         "get",
-    //         "name"
-    //       ]
-    //     ],
-    //     "text-font": [
-    //       "Noto Sans Regular"
-    //     ],
-    //     "text-letter-spacing": 0.1,
-    //     "text-rotation-alignment": "map",
-    //     "text-size": [
-
-    //       [
-    //         "zoom"
-    //       ],
-    //       10,
-    //       8,
-    //       16,
-    //       10,
-    //       24,
-    //       14
-    //     ],
-    //     "text-transform": "none",
-    //     "visibility": "visible"
-    //   }
-    // })
+    map.addLayer(
+      {
+        id: "cluster-count",
+        type: "symbol",
+        source: "development-activity-source",
+        filter: ["has", "point_count"],
+        layout: {
+          "text-field": "{point_count_abbreviated}",
+          "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+          "text-size": 12,
+        },
+      },
+      firstSymbolId
+    );
   });
+}
+
+function getFirstSymbol(map) {
+  const layers = map.getStyle().layers;
+
+  let firstSymbolId;
+  for (let i = 0; i < layers.length; i++) {
+    if (layers[i].type === "symbol") {
+      firstSymbolId = layers[i].id;
+      break;
+    }
+  }
+
+  return firstSymbolId;
 }
